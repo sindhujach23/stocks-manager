e0cd25bd8f0546babb7de4a8f8659f65f213c3bf
sindhujach23 <sindhuja.ch23@gmail.com>
2023-01-25 11:19:26 -0800

    Initial base project

---
+++ src/main/java/com/beassessment/stocksmanager/StocksManagerApplication.java
@@ -0,0 +1,13 @@
+package com.beassessment.stocksmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocksManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beassessment/stocksmanager/StocksManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beassessment.stocksmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
